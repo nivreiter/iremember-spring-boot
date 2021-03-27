ef4adc32ecccc54cfda4810ed1cf446640fe2e4c
Niv Reiter <nivreiter@gmail.com>
2021-03-27 16:56:21 +0300

    update

---
+++ prayer/src/main/java/com/iremember/prayer/PrayerApplication.java
@@ -0,0 +1,13 @@
+package com.iremember.prayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrayerApplication.class, args);
+	}
+
+}
---
+++ prayer/src/test/java/com/iremember/prayer/PrayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iremember.prayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
